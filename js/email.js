ba4a0e665d66f075ddee3cacf39cff8462022c3b
Wesley Cristiano Reis Silva <wesley.silva@gestcom.local>
2025-02-26 13:11:29 -0300

    first commig

---
+++ js/email.js
@@ -0,0 +1,10 @@
+
+document.getElementById("sendEmailButton").addEventListener("click", function() {
+    // Aqui você pode chamar uma função para enviar o email
+    sendEmail();
+});
+
+function sendEmail() {
+    // Lógica para enviar o email
+    alert("Email enviado com sucesso!");
+}
